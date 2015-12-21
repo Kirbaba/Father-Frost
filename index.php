616e56f33817c1de5afc3fe03e6f7c357ad32f35
Vladislav <v.kirbaba@gmail.com>
2015-12-22 00:32:07 +0300

    Финишь верстки

---
+++ index.php
@@ -0,0 +1,231 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="description" content="">
+    <link rel="icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE">
+    </script>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+	<article class="top-border"></article>
+
+	<section class="enterscreen">
+		<div class="contain">
+			<div class="enterscreen__text">
+				<h2><b>ДЕД МОРОЗ</b>  – НАСТОЯЩИЙ ПРАЗДНИК РЯДОМ С ВАМИ</h2>
+				<div class="enterscreen__text-item--1">
+					<p><b>Непередаваемое</b> ощущение праздника, воплощенное в реальности. <br>Богатый репертуар конкурсов</p>
+				</div>
+				<div class="enterscreen__text-item--2">
+					<p><b>Трезвый и опытный</b> артист в роли Деда Мороза. Яркий <b>запоминающийся</b> костюм героя.</p>
+				</div>
+				<div class="enterscreen__text-item--3">
+					<p><b>Индивидуальный подход</b> подход к каждому заказу. Возможность <b>быстрого заказа</b> артиста</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ourfrosts">
+		<div class="contain">
+			<h1>Наши Деды Морозы <span>самые дедоморозистые</span> в мире!</h1>
+			<div class="ourfrosts--video">
+				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/bGTQoh7CPXQ" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="ourfrosts--text">
+				<p>
+					Вы открываете двери, и яркий праздник 
+					наполняет атмосферу вашего торжества 
+					самыми позитивными красками. 
+					Живое воплощение сказочного героя 
+					рядом с вами превращает в феерию 
+					даже самые скромные посиделки. 
+					Неожиданные и приятные подарки 
+					из мешка Деда Мороза радуют и взрослых и детей. 
+					Веселые конкурсы с забавными комментариями 
+					от нашего волшебного дедушки увлекают всех в 
+					хоровод положительных эмоций.
+
+				</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="whyweare">
+		<div class="contain">
+			<h1>Почему  доверяют нам?</h1>
+			<div class="whyweare__item">
+				<div class="whyweare__item--img">
+					<img src="<?php bloginfo('template_directory'); ?>/img/trust1.png" alt="">
+				</div>
+				<h4>Наш Дед Мороз – остроумный 
+				и трезвый. </h4>
+				<p>Новый год – праздник для всей семьи. 
+				Поэтому наш дедушка не пьет. 
+				А быть веселым и добрым ему помогает 
+				новогоднее настроение и ваша благодарность.
+				</p>
+			</div>
+			<div class="whyweare__item">
+				<div class="whyweare__item--img">
+					<img src="<?php bloginfo('template_directory'); ?>/img/trust2.png" alt="">
+				</div>
+				<h4>Эксклюзивный традиционный 
+				костюм героя.
+				</h4>
+				<p>В нашем Деде Морозе 
+				прекрасно все - от бороды 
+				до посоха. Богатое и яркое убранство героя 
+				усиливает впечатление
+				 сказочности происходящего.</p>
+			</div>
+			<div class="whyweare__item">
+				<div class="whyweare__item--img">
+					<img src="<?php bloginfo('template_directory'); ?>/img/trust3.png" alt="">
+				</div>
+				<h4>В подарочном мешке нашего 
+				Деда Мороза много 
+				приятных сюрпризов.
+
+				</h4>
+				<p>В зависимости от характера и места проведения 
+				торжества наш артист подбирает 
+				самые подходящие конкурсы и развлечения.
+				</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="choice">
+			<div class="contain">
+				<h1>Выбирайте куда вы хотите пригласить Деда Мороза.</h1>
+				<div class="choice__item">
+					<div class="choice__item--number">
+						<b>1</b>
+					</div>
+					<div class="choice__item--text">
+						<p>Дед Мороз на рейде. <br>
+							Волшебный дедушка посетит ваш дом и в течение 5 минут <br>
+							поздравит всех его жителей и гостей.</p>
+					</div>
+				</div>
+
+				<div class="choice__item">
+					<div class="choice__item--number">
+						<b>2</b>
+					</div>
+					<div class="choice__item--text">
+						<p>Дед Мороз у вас в гостях. <br>
+							Сказочный герой поздравит вашего ребенка у вас дома <br>
+							и проведет небольшую программу с хороводом, танцами и конкурсами.
+							</p>
+					</div>
+				</div>
+
+				<div class="choice__item">
+					<div class="choice__item--number">
+						<b>3</b>
+					</div>
+					<div class="choice__item--text">
+						<p>Волшебный корпоратив. <br>
+						Дед Мороз посетит ваш праздник на предприятии или в ресторане <br>
+						со специальной программой, которую можно оговорить заранее.
+						</p>
+					</div>
+				</div>
+
+				<div class="choice__item">
+					<div class="choice__item--number">
+						<b>4</b>
+					</div>
+					<div class="choice__item--text">
+						<p>Утренник. <br>
+						Дед Мороз придет на детскую или взрослую елку, <br>
+						чтобы подарить всем ее участникам массу приятных и незабываемых впечатлений.
+						</p>
+					</div>
+				</div>
+				
+			</div>
+		</section>	
+
+		<section class="order">
+			<div class="contain">
+				<div class="order__item">
+					<h2>А вы хотите незабываемый праздник на этот Новый год? </h2>
+					<p>С нашим Дедом Морозом вы с головой окунаетесь в детство. 
+						А маленькие участники праздника будут потом целый год верить в чудо. 
+						Сказочный герой в волшебный праздник смены года может осуществлять 
+						самые смелые мечты. Мы помогаем поверить в себя и взрослым 
+						и детям. Доброта и внимание – главные правила вашего 
+						новогоднего праздника с нашим Дедом Морозом.
+					</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="saying">
+			
+			<div class="contain">
+				<h1>Что говорят?</h1>
+				<div class="fotorama" data-width="100%" data-height="275" data-loop="true" data-arrows="false" data-autoplay="true">
+					<div class="saying__item">
+						<div class="saying__item--img">
+							<img src="<?php bloginfo('template_directory'); ?>/img/girl.png" alt="">
+						</div>
+						<div class="saying__item--text">
+							<p>Мы пригласили деда Мороза на дом. <br>
+							Хотели поздравить дочку (ей 4 года). Сначала Светочка испугалась, 
+							но потом очень радовалась и с удовольствием танцевала вокруг елки (она у нас 
+							была искусственная). Понравилось даже коту. Он все время смотрел на деда Мороза, 
+							а в конце начал тереться об его валенки. Большое спасибо! 
+							</p>
+							<p><i>Ирина, домохозяйка.</i></p>
+						</div>
+					</div>
+
+					<div class="saying__item">
+						<div class="saying__item--img">
+							<img src="<?php bloginfo('template_directory'); ?>/img/girl.png" alt="">
+						</div>
+						<div class="saying__item--text">
+							<p>Мы пригласили деда Мороза на дом. <br>
+							Хотели поздравить дочку (ей 4 года). Сначала Светочка испугалась, 
+							но потом очень радовалась и с удовольствием танцевала вокруг елки (она у нас 
+							была искусственная). Понравилось даже коту. Он все время смотрел на деда Мороза, 
+							а в конце начал тереться об его валенки. Большое спасибо! 
+							</p>
+							<p><i>Ирина, домохозяйка.</i></p>
+						</div>
+					</div>
+				</div>
+			</div>			
+		</section>
+
+		<section class="write">
+			<div class="contain">
+				<div class="write__item">
+					<h3>Хотите Деда Мороза на свой праздник 
+						по специальной цене?</h3>
+					<h4>До ….. вы можете заказать Деда Мороза со скидкой.</h4>
+					<div class="write__arrow"></div>
+					<a href="#" class="write__but">Заказать</a>
+				</div>
+			</div>
+		</section>
+
+		<footer class="footer">
+			<div class="contain">
+				<h4>Сказка почти даром – специальная программа скидки. <br>
+				Зазывайте визит Деда Мороза сейчас, и цена услуги будет значительно меньше</h4>
+			</div>
+		</footer>
+	    
+<?php wp_footer(); ?>
+</body>
+</html>
